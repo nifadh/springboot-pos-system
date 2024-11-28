b743048e3ffcedbdfa725b351d4b12f49f7564c9
nifadh <mohamednifadh@gmail.com>
2024-11-29 00:05:03 +0530

    Created resource not found exception and handler for it

--- src/main/java/com/nifadh/pointofsales/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/nifadh/pointofsales/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.nifadh.pointofsales.exception.handler;
 
 import com.nifadh.pointofsales.exception.DuplicateResourceException;
+import com.nifadh.pointofsales.exception.ResourceNotFoundException;
 import com.nifadh.pointofsales.exception.dto.GeneralExceptionResponse;
 import com.nifadh.pointofsales.util.TimeUtil;
 import org.springframework.http.HttpStatus;
@@ -21,4 +22,17 @@ public class GlobalExceptionHandler {
                 .build();
         return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleException(ResourceNotFoundException ex) {
+        GeneralExceptionResponse response = GeneralExceptionResponse.builder()
+                .message(ex.getMessage())
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .timestamp(TimeUtil.getSriLankanDateTimeString())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+
 }
