4174aa604e9949b443070dfde8afc88106962043
nifadh <mohamednifadh@gmail.com>
2024-12-05 17:31:45 +0530

    Created Brand Request DTO

---
+++ src/main/java/com/nifadh/pointofsales/modules/product/brand/BrandRequest.java
@@ -0,0 +1,10 @@
+package com.nifadh.pointofsales.modules.product.brand;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class BrandRequest {
+    private final String name;
+}
