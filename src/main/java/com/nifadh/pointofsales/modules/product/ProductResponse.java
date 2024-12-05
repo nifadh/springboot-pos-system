ca93627e7d12586cbf9043b1b8e06f5278546420
nifadh <mohamednifadh@gmail.com>
2024-12-05 17:39:20 +0530

    Added a type BrandResponse field to include brand details

--- src/main/java/com/nifadh/pointofsales/modules/product/ProductResponse.java
+++ src/main/java/com/nifadh/pointofsales/modules/product/ProductResponse.java
@@ -1,5 +1,6 @@
 package com.nifadh.pointofsales.modules.product;
 
+import com.nifadh.pointofsales.modules.product.brand.BrandResponse;
 import com.nifadh.pointofsales.modules.product.category.Category;
 import com.nifadh.pointofsales.modules.product.category.CategoryResponse;
 import lombok.Builder;
@@ -15,6 +16,7 @@ public class ProductResponse {
     private final String name;
     private final String image;
     private final CategoryResponse category;
+    private final BrandResponse brand;
     private final Double costPrice;
     private final Double sellingPrice;
 }
